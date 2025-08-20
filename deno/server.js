e0e35a87c9bda4599abe1a2c28358c815e693b5b
SHO800 <86473181+SHO800@users.noreply.github.com>
2025-08-20 10:54:33 +0900

    fsRootをdeno/にしてみる

--- deno/server.js
+++ deno/server.js
@@ -4,20 +4,24 @@ import { serveDir } from 'jsr:@std/http@^1.0.17'
 /**
  * APIリクエストを処理する
  */
-Deno.serve((req) => {
+Deno.serve(async (req) => {
 	// URLのパスを取得
 	const pathname = new URL(req.url).pathname
-	console.log(pathname)
+	// console.log(pathname)
 	// パスが'/welcome-message'だったら「'jigインターンへようこそ！'」の文字を返す
 	if (req.method === 'GET' && pathname === '/welcome-message') {
 		return new Response('jig.jpインターンへようこそ！👍')
 	}
 
 	// publicフォルダ内にあるファイルを返す
-    return new Response(Deno.cwd(), )
-    // return new Response(Deno.readDirSync()
+    // return new Response(Deno.cwd(), )
+    // const files = await Deno.readDir('public')
+    // const fileNames = Array.from(files).map(file => file.name)
+    // console.log(fileNames)
+    // return new Response(" ")
+    
 	return serveDir(req, {
-		fsRoot: 'public',
+		fsRoot: 'deno/public',
 		urlRoot: '',
 		showDirListing: true,
 		enableCors: true,
